842f5094a709c724b0f51dd5be7841b3944afd2b
wendyji131 <31259905+wendyji131@users.noreply.github.com>
2018-04-18 21:41:31 -0700

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,13 +1,6 @@
+#pragma once
 #include <string>
 #include <fstream>
-//#include "lexer.h"
 using namespace std;
 
-#ifndef globals_h
-#define globals_h
-
 extern string outputFile;
-//extern vector<string> allWords;
-//extern unsigned tokenIndex;
-
-#endif /* globals_h */
